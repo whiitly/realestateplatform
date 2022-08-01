5c01660bf49df32db1b0ed27d707a246baa4adfb
gohjinzhu <gohjinzhu@hotmail.com>
2022-08-01 20:09:16 +0800

    search, detail pages search filters

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,25 @@
 import Router from 'next/router';
 import Head from 'next/head';
 import NProgress from 'nprogress';
-import {ChakraProvider} from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
 
 import Layout from '../components/Layout'
 
 function MyApp({ Component, pageProps }) {
+  NProgress.configure({ showSpinner: false })
+
+  Router.events.on('routeChangeStart', () => {
+    NProgress.start();
+  });
+
+  Router.events.on('routeChangeComplete', () => {
+    NProgress.done();
+  });
+
   return (
     <>
       <Head>
-
+        
       </Head>
       <ChakraProvider>
         <Layout>
--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home({ propertiesForSale, propertiesForRent }) {
           title2='Dream Home'
           desc1='Explore Apartments, Villas, Homes'
           desc2='and more'
-          buttonText='Explore Renting'
+          buttonText='Explore Buying'
           linkName="/search?purpose=for-sale"
           imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
         />
