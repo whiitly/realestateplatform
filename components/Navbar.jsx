23cf297a4d3e8751577b249bbe86d02928cd6482
gohjinzhu <94891192+gohjinzhu@users.noreply.github.com>
2022-08-04 13:38:25 +0800

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,7 +7,7 @@ import { FiKey } from 'react-icons/fi';
 const Navbar = () => (
     <Flex p='2' borderBottom='1px' borderColor='gray.100'>
         <Box fontSize='3xl' color='blue.400' fontWeight='bold'>
-            <Link href='/' padddingLeft='2'>NUS</Link>
+            <Link href='/' padddingLeft='2'>SGProperty</Link>
         </Box>
         <Spacer />
         <Box>
